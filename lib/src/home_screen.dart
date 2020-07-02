ff2a60f422b52aab453ac72d10c6869bc57b4bd0
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-02 15:52:54 -0300

    layout

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ragnarok/src/pages/column_card.dart';
+import 'package:ragnarok/src/widgets/drawer_list.dart';
 import 'package:ragnarok/src/widgets/search.dart';
+//import 'package:google_fonts/google_fonts.dart';
+//import 'dart:math' as math;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,15 +15,38 @@ class _HomeScreenState extends State<HomeScreen> {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: ListView(
-        padding: EdgeInsets.only(top: 80),
         children: <Widget>[
-          Text(
-            'What are you looking for ?',
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          Stack(
+            children: <Widget>[
+              IconButton(
+                iconSize: 32,
+                icon: Icon(Icons.format_list_bulleted),
+                color: Colors.grey.shade500,
+                //onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+              Positioned(
+                height: 260,
+                top: -55,
+                right: 20,
+                //child: Transform.rotate(angle: - math.pi + 3.6,
+                //child:
+                child:
+                    Image.asset('assets/images/globe.png', color: Colors.green),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 70),
+                child: Text(
+                  'What are you looking for ?',
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 16.0,
-          ),
+          SizedBox(height: 16.0),
           Search(),
           SizedBox(
             height: 40.0,
@@ -31,25 +57,31 @@ class _HomeScreenState extends State<HomeScreen> {
               children: <Widget>[
                 Row(
                   children: <Widget>[
-                    ColumnCard(img: 'assets/images/poring.png', text: 'Monsters'),
+                    ColumnCard(
+                        img: 'assets/images/poring.png', text: 'Monsters'),
                     SizedBox(width: 16),
-                    ColumnCard(img: 'assets/images/poring.png', text: 'Pets',),
+                    ColumnCard(
+                      img: 'assets/images/poring.png',
+                      text: 'Pets',
+                    ),
                   ],
                 ),
                 SizedBox(height: 16),
                 Row(
                   children: <Widget>[
-                    //ColumnCard(img: ''),
+                    ColumnCard(img: '', text: 'Items'),
                     SizedBox(width: 16),
-                    //ColumnCard(img: ''),
+                    ColumnCard(img: '', text: 'Cards'),
                   ],
                 ),
                 SizedBox(height: 16),
                 Row(
                   children: <Widget>[
-                    //ColumnCard(img: ''),
-                    SizedBox(width: 16,),
-                    //ColumnCard(img: ''),
+                    ColumnCard(img: '', text: 'Equipment'),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    ColumnCard(img: '', text: 'Maps',),
                   ],
                 )
               ],
@@ -64,6 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _body(),
+      drawer: DrawerList(),
     );
   }
 }
