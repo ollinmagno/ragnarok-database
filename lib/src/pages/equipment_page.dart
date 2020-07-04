1d492f4e8643d4b156ed699fc9cfac3351763a89
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-03 21:31:22 -0300

    appbar listview ou gridview

--- lib/src/pages/equipment_page.dart
+++ lib/src/pages/equipment_page.dart
@@ -7,6 +7,29 @@ class EquipmentPage extends StatefulWidget {
 }
 
 class _EquipmentPageState extends State<EquipmentPage> {
+  bool _gridView = true;
+
+  _body() {
+    if (_gridView) {
+      return GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+        //itemCount: equipments.length,
+        itemBuilder: (context, int index) {
+          //return
+        },
+      );
+    } else {
+      return ListView.builder(
+          itemExtent: 350,
+          //itemCount: equipments.length,
+          itemBuilder: (BuildContext context, int index) {
+            //return
+          }
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +37,28 @@ class _EquipmentPageState extends State<EquipmentPage> {
         title: Text('Equipments'),
         centerTitle: true,
         backgroundColor: appBarColor,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.grid_on, size: 20),
+            onPressed: () {
+              print("Lista");
+              setState(() {
+                _gridView = false;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.list, size: 26),
+            onPressed: () {
+              print("Grid");
+              setState(() {
+                _gridView = true;
+              });
+            },
+          )
+        ],
       ),
+      body: _body(),
     );
   }
 }
