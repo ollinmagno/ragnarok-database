ef880e906e2da7cc237fdbdee3822751628f3365
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-06 20:35:06 -0300

    formulário enviar feedback

--- lib/src/pages_drawer/send_feedback.dart
+++ lib/src/pages_drawer/send_feedback.dart
@@ -2,29 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:ragnarok/src/utils/colors.dart';
 
 class SendFeedback extends StatelessWidget {
-  _body(){
-    return SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextFormField(
+  GlobalKey _keyForm = GlobalKey<FormState>();
+  final TextEditingController _controllerTitle = TextEditingController();
+  final TextEditingController _controllerSendFeedback = TextEditingController();
 
-            ),
-            SizedBox(height: 16),
-            TextFormField(),
-          ],
+  _body() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(top: 45, left: 16.0, right: 16.0, bottom: 16.0),
+      child: Form(
+        key: _keyForm,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                width: 330,
+                child: TextFormField(
+                  controller: _controllerTitle,
+                  maxLength: 30,
+                  keyboardType: TextInputType.text,
+                  maxLines: 1,
+                  //onChanged: (texto) {},
+                  decoration: InputDecoration(
+                    counterText: '',
+                    labelText: 'Title',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1.0),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 60),
+              SizedBox(
+                width: 330,
+                child: TextFormField(
+                  controller: _controllerSendFeedback,
+                  maxLength: 900,
+                  keyboardType: TextInputType.text,
+                  maxLines: 20,
+                  //onChanged: (texto) {},
+                  decoration: InputDecoration(
+                    counterText: '',
+                    labelText: '',
+                    labelStyle: TextStyle(height: 0.0, color: Colors.red),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1.0),
+                    ),
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('About'),
+        title: Text('Feedback'),
         centerTitle: true,
         backgroundColor: appBarColor,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.send), onPressed: (){}),
+        ],
       ),
       body: _body(),
     );
