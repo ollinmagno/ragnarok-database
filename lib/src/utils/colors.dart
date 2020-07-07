204b13ea1af183364418154bdf4ccc3401cc2b30
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-07 17:24:46 -0300

    add color

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -3,3 +3,8 @@ import 'dart:ui';
 
 const appBarColor = const Color(0xfff8e7d3);
 const drawerColor = const Color(0xfff8e7d3);
+
+
+const colorButton = const Color(0xfff8e7d3);
+const colorButtonConfirm = const Color(0xfff7ca97);
+const colorButtonSelect = const Color(0xfff7ca97);
