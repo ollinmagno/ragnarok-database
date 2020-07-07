b196a3123569f35ef745f43985977f84d4aa3155
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-07 17:24:33 -0300

    alert dialog - filtro

--- lib/src/widgets/search.dart
+++ lib/src/widgets/search.dart
@@ -33,11 +33,70 @@ class Search extends StatelessWidget {
               iconSize: 32,
               icon: Icon(Icons.filter_list),
               color: Colors.grey.shade500,
-              onPressed: () {},
+              onPressed: () {
+                print('filter');
+                return _onClickDialog(context);
+              },
             ),
           ],
         ),
       ),
     );
+  }
+
+   _onClickDialog(context) {
+    showDialog(context: context, builder: (context){
+      return Center(
+        child: Material(
+          color: Colors.white,
+          elevation: 14.0,
+          borderRadius: BorderRadius.circular(24.0),
+          child: Container(
+            width: 285,
+            height: 300,
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Filter',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Text(
+                      'categories',
+                      style: TextStyle(color: Colors.black, fontSize: 18.0),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 32.0),
+                  child: RaisedButton(
+                      color: Color(0xfff7ca97),
+                      child: new Text(
+                        'Confirm',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0))),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
