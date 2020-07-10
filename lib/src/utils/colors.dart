8b66ef5fb1eb6e9d13e28abf2b4c21f97348b0ef
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-10 13:09:10 -0300

    add color container

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 
 const primaryColor = const Color(0xfff8e7d3);
-const drawerColor = const Color(0xfff8e7d3);
+const colorContainer = const Color(0xFF8BC34A);
 
 const colorButton = const Color(0xfff8e7d3);
 const colorButtonSelect = const Color(0xfff7ca97);
