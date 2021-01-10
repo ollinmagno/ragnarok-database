3b501cf4bdb0767bc8ae0cf017a03c55836f9826
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-01-10 15:40:41 -0300

    rota para card

--- lib/src/routes/router.dart
+++ lib/src/routes/router.dart
@@ -1,30 +1,16 @@
 import 'package:flutter/cupertino.dart';
-import 'package:ragnarok/src/home_screen.dart';
-import 'package:ragnarok/src/pages/equipment_page.dart';
-import 'package:ragnarok/src/pages/items_page.dart';
-import 'package:ragnarok/src/pages/maps_page.dart';
-import 'package:ragnarok/src/pages/monsters_page.dart';
-import 'package:ragnarok/src/pages/page_of_letters.dart';
-import 'package:ragnarok/src/pages/pets_page.dart';
+import 'package:ragnarok/src/views/home_screen.dart';
 import 'package:ragnarok/src/routes/routes.dart';
+import 'package:ragnarok/src/views/select_card.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case homeScreen:
       return CupertinoPageRoute(builder: (context) => HomeScreen());
-    case equipmentPage:
-      return CupertinoPageRoute(builder: (context) => EquipmentPage());
-    case itemsPage:
-      return CupertinoPageRoute(builder: (context) => ItemsPage());
-    case mapsPage:
-      return CupertinoPageRoute(builder: (context) => MapsPage());
-    case monsterPage:
-      return CupertinoPageRoute(builder: (context) => MonstersPage());
-    case pageOfLetters:
-      return CupertinoPageRoute(builder: (context) => PageOfLetters());
-    case petsPage:
-      return CupertinoPageRoute(builder: (context) => PetsPage());
-      break;
+//    case equipmentPage:
+//      return CupertinoPageRoute(builder: (context) => EquipmentPage());
+    case selectCard:
+      return CupertinoPageRoute(builder: (context) => SelectCard());
     default:
       return CupertinoPageRoute(builder: (context) => HomeScreen());
   }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,2 @@
 const String homeScreen = '/';
-const String equipmentPage = 'equipment';
-const String itemsPage = 'items';
-const String mapsPage = 'maps';
-
-const String monsterPage = 'monsters';
-const String pageOfLetters = 'letters';
-const String petsPage = 'pets';
+const String selectCard = 'select_card';
